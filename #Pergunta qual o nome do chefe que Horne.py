130124372286d2d8e7f603930e351aa46465bd30
Kaike da Paz <kaikedapaz967@gmail.com>
2026-04-23 09:50:28 -0300

    Add files via upload

---
+++ #Pergunta qual o nome do chefe que Horne.py
@@ -0,0 +1,36 @@
+#Pergunta qual o nome do chefe que Hornet batalhará
+nome_chefe = input()
+if nome_chefe == "Tessela":
+    print("Tessela: Ha Ha Ha! Parece que a aranha retornou.")
+elif nome_chefe == "Grande Mãe Seda":
+    print("Hornet: Monarca, seu reino de tirania acaba aqui!")
+elif nome_chefe == "A Última Juíza":
+    print("Hornet: Não posso recuar agora, a cidadela está logo ali.")
+else:
+    print(f"Hornet: {nome_chefe}, levante sua lâmina!")
+print()
+
+#Estados
+hornet_estado = "viva"
+chefe_estado = "vivo"
+
+qtd_mascaras = 5
+carretel_de_seda = 0
+vida_do_chefe = 140
+
+def ação_hornet(a1):
+
+
+def ação_chefe (a2):
+
+def sistema_de_batalha ():
+
+while hornet_estado == "viva" and carretel_de_seda == "vivo":
+    x = input()
+    ação_hornet(x)
+    y = input()
+    ação_chefe(y)
+
+
+
+   
